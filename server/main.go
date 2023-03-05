7b87134349133b2889e6fb961bb17df3e491dea9
ZillaZ <lucasmula125@gmail.com>
2023-03-05 07:47:02 -0300

    Performance Improvements

--- server/main.go
+++ server/main.go
@@ -26,73 +26,71 @@ func main() {
   if err != nil {
     panic("error accessing db")
   }
+
   db.AutoMigrate(&Task {})
 
+  tasks := init_tasks(db)
+
   e.GET("/", func(c echo.Context) error {
-		var tasks = get_tasks()
-    return c.JSON(http.StatusOK, tasks)
+    t := map_to_arr(tasks)
+    return c.JSON(http.StatusOK, t)
 	})
 
   e.GET("/:desc", func(c echo.Context) error {
-    update_task(c.Param("desc"))
+    update_task(db, c.Param("desc"), tasks)
     return c.String(http.StatusOK, "OK")
   })
 
   e.GET("/new/:desc", func(c echo.Context) error {
-    add_task(c.Param("desc"))
+    add_task(db, c.Param("desc"), tasks)
     return c.String(http.StatusOK, "OK")
   })
 
   e.GET("/del/:desc", func(c echo.Context) error {
-    del_task(c.Param("desc"))
+    del_task(db, c.Param("desc"), tasks)
     return c.JSON(http.StatusOK, "OK")
   })
 
 	e.Logger.Fatal(e.Start(":5050"))
 }
 
-func update_task(t string) {
-  db, err := gorm.Open(sqlite.Open("todo.db"), &gorm.Config{})
-
-  if err != nil {
-    panic("error accessing db")
-  }
-
+func update_task(db *gorm.DB, desc string, tasks map[string]bool) {
   var task Task
-  db.First(&task, "Description = ?", string(t))
-  db.Model(&task).Update("Completed", !task.Completed)
+  tasks[desc] = !tasks[desc]
+  go db.First(&task, "Description = ?", string(desc))
+  go db.Model(&task).Update("Completed", !task.Completed)
 }
 
-func add_task(desc string)  {
-  db, err := gorm.Open(sqlite.Open("todo.db"), &gorm.Config {})
-
-  if err != nil {
-    panic("error accessing db")
-  }
-
-  var task Task = Task {Description: desc, Completed: false}
-  db.Create(&task)
+func add_task(db  *gorm.DB, desc string, tasks map[string]bool)  {
+  tasks[desc] = false
+  task := Task { Description: desc, Completed: false }
+  go db.Create(&task)
 }
 
-func get_tasks() []Task {
-  db, err := gorm.Open(sqlite.Open("todo.db"), &gorm.Config{})
-
-  if err != nil {
-    panic("error accessing db")
-  }
-
+func init_tasks(db *gorm.DB) map[string]bool {
   var tasks [] Task
   db.Find(&tasks)
-  return tasks
+
+  m := make(map[string]bool)
+  
+  for _, val := range tasks {
+    m[val.Description] = val.Completed
+  }
+  return m
 }
 
-func del_task(desc string) {
-  db, err := gorm.Open(sqlite.Open("todo.db"), &gorm.Config{})
-
-  if err != nil {
-    panic("error accessing db")
-  }
-
+func del_task(db *gorm.DB, desc string, tasks map[string]bool) {
   var task Task = Task { Description: desc, Completed: true }
-  db.Where("Description = ?", desc).Delete(&task)
+  delete(tasks, desc)
+  go db.Where("Description = ?", desc).Delete(&task)
+}
+
+func map_to_arr(tasks map[string]bool) []Task {
+  t := make([]Task, len(tasks))
+  var i = 0
+  for key, value := range tasks {
+    t[i] = Task{ Description: key, Completed: value }
+    i+=1
+  }
+  return t
 }
